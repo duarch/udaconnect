52818730d65f01d23de4c31bee65a806811a91cc
duarch <@users.noreply.github.com>
2021-11-16 19:17:43 -0300

    Updating files

--- modules/api/app/udaconnect/__init__.py
+++ modules/api/app/udaconnect/__init__.py
@@ -1,12 +1,8 @@
 from app.udaconnect.models import Connection, Location, Person  # noqa
 from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema  # noqa
-from modules.location_srv.app.udaconnect.models import Location
-from modules.location_srv.app.udaconnect.schemas import LocationSchema
 
 
 def register_routes(api, app, root="api"):
     from app.udaconnect.controllers import api as udaconnect_api
-    from modules.location_srv.app.udaconnect.controllers import loc as location_loc 
 
     api.add_namespace(udaconnect_api, path=f"/{root}")
-    
