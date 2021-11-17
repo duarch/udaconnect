7dd8afe855b3f29f4b3085989fac0936025b126c
duarch <@users.noreply.github.com>
2021-11-17 19:04:48 -0300

    fix locantion pod

--- modules/location/app/udaconnect/services.py
+++ modules/location/app/udaconnect/services.py
@@ -111,24 +111,24 @@ class LocationService:
         return new_location
 
 
-# class PersonService:
-#     @staticmethod
-#     def create(person: Dict) -> Person:
-#         new_person = Person()
-#         new_person.first_name = person["first_name"]
-#         new_person.last_name = person["last_name"]
-#         new_person.company_name = person["company_name"]
+class PersonService:
+    @staticmethod
+    def create(person: Dict) -> Person:
+        new_person = Person()
+        new_person.first_name = person["first_name"]
+        new_person.last_name = person["last_name"]
+        new_person.company_name = person["company_name"]
 
-#         db.session.add(new_person)
-#         db.session.commit()
+        db.session.add(new_person)
+        db.session.commit()
 
-#         return new_person
+        return new_person
 
-#     @staticmethod
-#     def retrieve(person_id: int) -> Person:
-#         person = db.session.query(Person).get(person_id)
-#         return person
+    @staticmethod
+    def retrieve(person_id: int) -> Person:
+        person = db.session.query(Person).get(person_id)
+        return person
 
-#     @staticmethod
-#     def retrieve_all() -> List[Person]:
-#         return db.session.query(Person).all()
+    @staticmethod
+    def retrieve_all() -> List[Person]:
+        return db.session.query(Person).all()
