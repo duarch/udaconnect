2511f7f6e868d4341f2f7d493ec867646610141c
duarch <@users.noreply.github.com>
2021-11-17 19:14:52 -0300

    fix loc pod

--- modules/location/app/udaconnect/controllers.py
+++ modules/location/app/udaconnect/controllers.py
@@ -37,28 +37,28 @@ class LocationResource(Resource):
         return location
 
 
-# @api.route("/persons")
-# class PersonsResource(Resource):
-#     @accepts(schema=PersonSchema)
-#     @responds(schema=PersonSchema)
-#     def post(self) -> Person:
-#         payload = request.get_json()
-#         new_person: Person = PersonService.create(payload)
-#         return new_person
+@api.route("/persons")
+class PersonsResource(Resource):
+    @accepts(schema=PersonSchema)
+    @responds(schema=PersonSchema)
+    def post(self) -> Person:
+        payload = request.get_json()
+        new_person: Person = PersonService.create(payload)
+        return new_person
 
-#     @responds(schema=PersonSchema, many=True)
-#     def get(self) -> List[Person]:
-#         persons: List[Person] = PersonService.retrieve_all()
-#         return persons
+    @responds(schema=PersonSchema, many=True)
+    def get(self) -> List[Person]:
+        persons: List[Person] = PersonService.retrieve_all()
+        return persons
 
 
-# @api.route("/persons/<person_id>")
-# @api.param("person_id", "Unique ID for a given Person", _in="query")
-# class PersonResource(Resource):
-#     @responds(schema=PersonSchema)
-#     def get(self, person_id) -> Person:
-#         person: Person = PersonService.retrieve(person_id)
-#         return person
+@api.route("/persons/<person_id>")
+@api.param("person_id", "Unique ID for a given Person", _in="query")
+class PersonResource(Resource):
+    @responds(schema=PersonSchema)
+    def get(self, person_id) -> Person:
+        person: Person = PersonService.retrieve(person_id)
+        return person
 
 
 @api.route("/persons/<person_id>/connection")
