d1f04f0cdd54cf234fd02ff5bb30315ab2bf73e7
duarch <@users.noreply.github.com>
2021-12-25 12:51:51 -0300

    New comsumer version

--- modules/consumer/app/udaconnect/consumer.py
+++ modules/consumer/app/udaconnect/consumer.py
@@ -1,31 +1,30 @@
 from kafka import KafkaConsumer
-import logging
 import os
 import json
 from sqlalchemy import create_engine
 
-kafka_dns = os.environ["DNS"]
-topic_name = os.environ["TOPIC"]
+# Setting Constants
 DB_USERNAME = os.environ["DB_USERNAME"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
+KAFKA_URL = os.environ["KAFKA_URL"]
+TOPIC_NAME = os.environ["KAFKA_TOPIC"]
 
-logging.info('connecting to kafka ', kafka_dns)
-logging.info('connecting to kafka topic ', topic_name)
-consumer = KafkaConsumer(topic_name, bootstrap_servers=[kafka_dns])
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=[KAFKA_URL])
 
-def write_to_db(location):
+# Consuming data 
+def collect_data(location):
     conn = create_engine(f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
     cur = conn.cursor()
     user_id = int(location["userId"])
     latitude, longitude = int(location["latitude"]), int(location["longitude"])
-    table_insert = "INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}))" \
+    db_data = "INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}))" \
         .format(user_id, latitude, longitude)
 
-    print(table_insert)
-    cur.execute(table_insert)
+    print(db_data)
+    cur.execute(db_data)
 
 
 for topic in consumer:
